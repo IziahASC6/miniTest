9068bac8f30a80c36958f5a9b09983df3071bd0d
IziahASC6 <iziahhouse@gmail.com>
2019-07-22 16:05:00 -0400

    miniTest

--- test.js
+++ test.js
@@ -1,19 +1,25 @@
 // 1. Declare a number called myNumber that is equal to your age and print it
-
+let myNumber = 16
+console.log(myNumber)
 
 
 // 2. Declare a string called myName that contains your name and print it
 
-
+let myName = ["Iziah"]
+console.log(myName)
 
 // 3. Declare an array named myArray that contains your age, your first name, and if it's true or false that you're a student
 
-
+let myArray= ["16","Iziah","True"]
+console.log (myArray)
 
 // 4. Write and call a function called printHello that prints your name to the console by accessing myArray
 
+function printHello(){
+    let (myArray) = ["16","Iziah","True"]
+}
 
-
+printHello()
 /* 5.
 1)create a function called area that takes in two parameters, width and length
 2) the function, area should return width times length
@@ -39,23 +45,23 @@ let nestedArray = [
 
 //9. Command line problems (MAKE SURE TO ANSWER THESE IN THE COMMENTS)
 
-//9a. make a directory called test
+//9a. make a directory called test 
 
-// write answer here: 
+// write answer here: mkdir test
 
-//9b. move to your test directory that you just created
+//9b. move to your test directory that you just created 
 
-// write answer here: 
+// write answer here: mv {test} {documents}
 
 //9c. make a file called test.js
 
-// write answer here: 
+// write answer here: touch test.js
 
 //9d. what is the command to open a file to vscode? 
 
-// write answer here: 
+// write answer here: "code" 
 
 //9e. So you already did "git add ." and then you did git commit -m
 // What is the last step of the process to push everything to github?
 
-// write answer here: 
+// write answer here:git push  
